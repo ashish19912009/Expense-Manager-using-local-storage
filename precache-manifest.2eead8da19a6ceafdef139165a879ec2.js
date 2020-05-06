ba806aa114af308a3b6ef8dec8c0c585bee72c16
ashish kumar rena <ashishkumarrena@ashishs-MacBook-Air.local>
2020-05-06 13:39:07 +0530

    Updates

---
+++ precache-manifest.2eead8da19a6ceafdef139165a879ec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c34ff873eec59a956e2586ff56792ebe",
+    "url": "/Expense-Manager-using-local-storage/index.html"
+  },
+  {
+    "revision": "4a02c214e0855ce8c7ba",
+    "url": "/Expense-Manager-using-local-storage/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1c9fc673e6c25680e66a",
+    "url": "/Expense-Manager-using-local-storage/static/js/2.79ec6a91.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Expense-Manager-using-local-storage/static/js/2.79ec6a91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a02c214e0855ce8c7ba",
+    "url": "/Expense-Manager-using-local-storage/static/js/main.dd1b7c83.chunk.js"
+  },
+  {
+    "revision": "98847e59c2f9aeb84ab5",
+    "url": "/Expense-Manager-using-local-storage/static/js/runtime-main.ebf6ae03.js"
+  }
+]);
